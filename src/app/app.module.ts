6d1256bdb4287c762ca1b6da4a8c33868c554429
thedung <thedung@gmail.com>
2022-09-21 19:59:29 +0700

    up code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,26 @@ import {ClassComponent} from "./classroom/class.component";
 import {FormsModule} from "@angular/forms";
 import {HomeComponent} from "./pages/home/home.component";
 import {LoginComponent} from "./pages/login.component/login.component";
+import {RouterModule, Routes} from "@angular/router";
+import {ThedungComponent} from "./pages/thedung/thedung.component";
+import {ContactUsComponent} from "./pages/contactUs/contactUs.component";
+import {AboutUsComponent} from "./pages/aboutUs/aboutUs.component";
 
 // khai bao cac pages trong website
+const appRoutes: Routes = [
+  {path:'',component: HomeComponent},
+  {path:'login', component: LoginComponent},
+  {path:'thedung', component: ThedungComponent},
+  {path:'contactUs', component: ContactUsComponent},
+  {path:'aboutUs', component: AboutUsComponent}
+
+]
 @NgModule({
   declarations: [
-    AppComponent, StudentComponent, ClassComponent, HomeComponent, LoginComponent
+    AppComponent, StudentComponent, ClassComponent, HomeComponent, LoginComponent, ThedungComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/thedung/thedung.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-thedung',
+  templateUrl: './thedung.component.html'
+})
+export class ThedungComponent extends  Component{
+
+}
