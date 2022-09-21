69d0940c8950188f1ac3fefc7defa237e47fdb71
thedung <thedung@gmail.com>
2022-09-21 18:53:18 +0700

    up code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {StudentComponent} from "./student/student.component";
 import {ClassComponent} from "./classroom/class.component";
+import {FormsModule} from "@angular/forms";
+import {HomeComponent} from "./pages/home/home.component";
+import {LoginComponent} from "./pages/login.component/login.component";
 
+// khai bao cac pages trong website
 @NgModule({
   declarations: [
-    AppComponent, StudentComponent, ClassComponent
+    AppComponent, StudentComponent, ClassComponent, HomeComponent, LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
