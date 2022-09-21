99e6e395d3495f9c3410d709dc9b70ba9bc54807
thedung <thedung@gmail.com>
2022-09-21 21:24:24 +0700

    up code

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import {Component} from "@angular/core";
+import {IStudent} from "../../interfaces/student.interface";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent extends  Component{
-
+  student: IStudent = {id:1, name:'TheDung', age:19, address:'Yen Hoa Parkview'};
 }
